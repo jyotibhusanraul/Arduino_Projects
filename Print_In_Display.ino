110dd2bec1cb81df1cb7ddc03e16be99751fd260
jyotibhusanraul <jyotibhusan.r@gmail.com>
2020-09-13 21:26:36 +0530

    Remove UnderScore_

--- Print_In_Display.ino
+++ Print_In_Display.ino
@@ -14,5 +14,5 @@ void loop() {
   // set the cursor to column 0, line 1
   // (note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
-  lcd.print("I'm Jyoti_Raul");
+  lcd.print("I'm Jyoti Raul");
 }
